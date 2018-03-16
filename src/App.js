03ec797b65f31d576f902a412607a74de2514a37
Antoine Jaussoin <antoine.jaussoin@gmail.com>
2018-03-16 11:15:04 +0000

    [Week 07] - Creating the Card Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Card from './Components/Card';
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hello World
+        <Card>1</Card>
+        <Card>2</Card>
+        <Card>3</Card>
+        <Card>🍎</Card>
       </div>
     );
   }
