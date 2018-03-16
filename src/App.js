8e8d3eba0ee12d5528633bdecdf64db9c71fdbf8
Antoine Jaussoin <antoine.jaussoin@gmail.com>
2018-03-16 11:40:19 +0000

    [Week 07] - Making the home page nicer

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Card from './Components/Card';
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <Card>1</Card>
         <Card color="yellow">2</Card>
         <Card color="#8bc34a">3</Card>
