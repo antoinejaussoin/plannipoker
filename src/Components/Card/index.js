a80c2bd61a284436cf9f8f0de1e75d6ab2d97a57
Antoine Jaussoin <antoine.jaussoin@gmail.com>
2018-03-16 11:26:03 +0000

    [Week 07] - Styling the Card

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import './index.css';
 
 const Card = ({ children }) => (
   <div className="card">
-    {children}
+    <div className="content">
+      {children}
+    </div>
   </div>
 );
 
