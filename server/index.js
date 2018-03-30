76a9ffefcaa39586daa6865c6c64052c234e3af0
Antoine Jaussoin <antoine.jaussoin@gmail.com>
2018-03-30 09:41:31 +0100

    [Week 09] - Adding cards endpoint and fixing proxy

--- server/index.js
+++ server/index.js
@@ -3,8 +3,22 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get('/api/test', (req, res) => {
-  res.send({ plannipoker: 'Response from server' });
+app.get('/api/cards', (req, res) => {
+  res.send([
+    { value: 0, label: '0', color: '#66bb6a' },
+    { value: 0.5, label: '0', color: '#66bb6a' },
+    { value: 1, label: '1', color: '#66bb6a' },
+    { value: 2, label: '2', color: '#66bb6a' },
+    { value: 3, label: '3', color: '#03a9f4' },
+    { value: 5, label: '5', color: '#03a9f4' },
+    { value: 8, label: '8', color: '#03a9f4' },
+    { value: 13, label: '13', color: '#ba68c8' },
+    { value: 20, label: '20', color: '#ba68c8' },
+    { value: 40, label: '40', color: '#ba68c8' },
+    { value: 100, label: '100', color: '#ba68c8' },
+    { value: 0, label: '?', color: '#c2185b' },
+    { value: 0, label: '☕️', color: '#c2185b' },
+  ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
