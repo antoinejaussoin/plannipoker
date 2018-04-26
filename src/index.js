2586cfae1095f215889cf4b53637b6321efa2350
Antoine Jaussoin <antoine.jaussoin@gmail.com>
2018-04-26 21:27:50 +0100

    [Week 13] - Adding React Router

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import 'whatwg-fetch';
 import './global-styles';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
