fc29d7884e5883f5012ec9cc5bb3ff882f411e64
Antoine Jaussoin <antoine.jaussoin@gmail.com>
2018-06-08 10:43:04 +0100

    [Week 19] - Restyling home page

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -2,16 +2,36 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import shortid from 'shortid';
+import { Typography, Button } from '@material-ui/core';
+import { VideogameAsset } from '@material-ui/icons';
 
-const Page = styled.div``;
+const Page = styled.div`
+  text-align: center;
+  margin-top: 50px;
+
+  a {
+    margin-top: 30px;
+  }
+`;
+
+const StartGameLink = (props) => <Link to={`/games/${shortid.generate()}`} {...props} />;
+
+const IconButton = styled(Button)`
+  svg {
+    margin-left: 10px;
+  }
+`;
 
 class Home extends Component {
   render() {
     return (
       <Page>
-        <h1>Welcome to Plannipoker</h1>
+        <Typography variant="display2" gutterBottom align="center">Welcome to Plannipoker</Typography>
 
-        <Link to={`/games/${shortid.generate()}`}>Start a new game!</Link>
+        <IconButton variant="raised" color="secondary" component={StartGameLink}>
+          Start a new game
+          <VideogameAsset  />
+        </IconButton>
       </Page>
     );
   }
